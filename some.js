883b0e36057ea145ca87245b5e01d9083192d7da
andrew-sumy <faandrewfa@gmail.com>
2020-04-14 21:30:29 +0300

    test

--- some.js
+++ some.js
@@ -1,3 +1,7 @@
+var http = require("http");
 
-console.log('dafs');
-
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(8888);ex
