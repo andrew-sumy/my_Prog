cb54c95a91fdb30a96788d72b3778182dac99422
Anton Tiptyuk <anton.tiptyuk@gmail.com>
2020-05-01 19:05:37 +0300

    4 approx

--- serialize/serialize.js
+++ serialize/serialize.js
@@ -1,34 +1,37 @@
-const doStuff = obj2Flatten => {
+const flattenTag = (path, obj) => {
+  const result = [];
 
-  const flattenTag =
-    (path, obj) => {
-      const result = [];
+  Object
+    .keys(obj)
+    .forEach(key => {
+      const val = obj[key];
+      const xType = typeof val;
 
-      Object
-        .keys(obj)
-        .forEach(key => {
-          const val = obj[key];
-          const xType = typeof val;
-          const valPath = `${path}.${key}`;
+      // const valPath =
+      //   Array.isArray(obj)
+      //     ?
+      //     `${path}[${key}]`
+      //     :
+      //     `${path}.${key}`;
+      const valPath = `${path}${Array.isArray(obj) ? `[${key}]` : `.${key}`}`;
 
-          switch (xType) {
-            case 'string':
-              result.push(`${valPath} = "${val}"`);
-              break;
-            case 'number':
-              result.push(`${valPath} = ${val}`);
-              break;
-            case 'object':
-              result.push(...flattenTag(valPath, val));
-          }
-        });
 
-      return result;
-    }
+      switch (xType) {
+        case 'string':
+          result.push(`${valPath} = "${val}"`);
+          break;
+        case 'number':
+          result.push(`${valPath} = ${val}`);
+          break;
+        case 'object':
+          result.push(...flattenTag(valPath, val));
+      }
+    });
 
-  // console.log(flattenTag('root>', obj2Flatten));
+  return result;
+}
 
-  return flattenTag('root>', obj2Flatten).join('\n');
-};
+const doStuff = obj2Flatten =>
+  flattenTag('root>', obj2Flatten).join('\n');
 
 module.exports = { doStuff };
