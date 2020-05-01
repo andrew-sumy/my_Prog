156bdd8f4a299b05401935b3c38abbf90c5da42a
Anton Tiptyuk <anton.tiptyuk@gmail.com>
2020-05-01 18:27:04 +0300

    0 approx

--- serialize/serialize.js
+++ serialize/serialize.js
@@ -1,5 +1,14 @@
 const doStuff = obj2Flatten => {
-  console.log('doStuff');
+
+  const result = [];
+
+  for (const x in obj2Flatten)
+    result.push(`${x} <${typeof obj2Flatten[x]}>`);
+  // console.log(x, typeof obj2Flatten[x], obj2Flatten[x]);
+
+  // console.log(result);
+  console.log(result.join('\n'));
+
   return 'obj.prop[idx].nestedProp = "val"';
 };
 
