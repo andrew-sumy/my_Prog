25191de1c978a1ca6ded5424ee5bfd9f99277d75
andrew-sumy <faandrewfa@gmail.com>
2020-05-02 21:32:20 +0300

    start my

---
+++ my.js
@@ -0,0 +1,9 @@
+let my = ['the fix of mobile telephon', 
+'the fix of monile telephon',
+'the fix of mobile telefon',
+'th fix of mobile telefon',
+'rte fixe of movile telefon',
+'the fix of mobile telephon',
+'the try to fix talet']
+
+
