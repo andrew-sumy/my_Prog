4e9d6fd309dbdbf820b16c1e5e73fc9345895605
andrew-sumy <faandrewfa@gmail.com>
2020-05-07 23:21:03 +0300

    to del

---
+++ draft.js
@@ -0,0 +1,14 @@
+function map(qq, a) {
+    var result = []; // Create a new Array
+    var i; // Declare variable
+    for (i = 0; i != a.length; i++)
+        result[i] = qq(a[i]);
+    return result;
+}
+var f = function (x) {
+    return x * x * x;
+}
+var numbers = [0, 1, 2, 5, 10];
+var cube = map(f, numbers);
+console.log(cube);
+console.log(numbers.map(f));
